e21d637e74cb3af58e95cb94b6dc96cfb956eb61
Ravinder Soni <sonirav@gmail.com>
2017-05-07 03:18:12 -0700

    Node software with input and list options

---
+++ liri.js
@@ -0,0 +1,203 @@
+// Node Software ---- By Ravinder Soni
+// Defining the global variables
+var fs = require("fs");
+var ky = require("./keys.js");
+var inquirer = require("inquirer");
+var action = process.argv;
+var newSearch = "";
+var request = require("request");
+var myChoices;
+// Clearing the screen
+process.stdout.write('\033c');
+myLoop();  // Calling Menu Option
+//  *********************** Calling Request
+
+function myLoop() { //It is continues call for the menu till user selects exit
+
+    inquirer.prompt([{
+        type: "list",
+        name: "myChoices",
+        message: "------(MENU)------",
+        choices: ["Movies", "Spotify", "Tweets", "Exit"] // Menu to Choose Options
+
+    }, ]).then(function(user) {
+        console.log(user.myChoices);
+        if (user.myChoices == "Movies") {
+
+            action[2] = "movies-this";
+
+            checkarg("1"); // formatting the command line search 
+
+
+        } else
+        if (user.myChoices == "Spotify") {
+
+            action[2] = "spotify-this-song";
+
+            checkarg("2"); // formatting the command line search 
+
+        } else
+        if (user.myChoices == "Tweets") {
+
+            action[2] = "my-tweets";
+            chktwitter();
+        } else
+        if (user.myChoices == "Exit") {
+
+            process.exit();
+        }
+
+    });
+
+
+
+}
+
+//**************************************** Twitter Search Function
+function chktwitter() {
+    var Twitter = require('twitter');
+    var client = new Twitter({
+    
+        consumer_key:ky.consumer_key,
+        consumer_secret:ky.consumer_secret,
+        access_token_key:ky.access_token_key,
+        access_token_secret:ky.access_token_secret,
+    });
+
+    var params = {
+        'screen_name': 'ravithecoder'
+
+    };
+
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            var cnt = tweets[0].user.statuses_count;
+
+            console.log("***************MY TWITTER***************");
+            console.log("Name : " + tweets[0].user.name);
+            console.log("Screen Name :@" + tweets[0].user.screen_name);
+            console.log("Account Created On :" + tweets[0].user.created_at);
+            console.log("Following :" + tweets[0].user.friends_count + " friends.");
+
+            for (var i = 0; i < cnt; i++) {
+                var tcount = i + 1;
+                console.log("---------------My Tweet (" + tcount + ")--------------");
+                console.log(tweets[i].text);
+                console.log("........................................");
+                console.log(tweets[i].created_at);
+                console.log("----------------------------------------");
+            }
+
+        } else {
+            console.log(error);
+        }
+        myLoop();
+    });
+
+}
+
+
+
+//********************************* Movie Search Function
+function chkmovie(newSearch, request) {
+    process.stdout.write('\033c');
+    if (newSearch == "") {
+        newSearch = "Mr.Nobody";
+
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + newSearch + "&y=&plot=short&r=json";
+    //	console.log(queryUrl);
+    var data = request(queryUrl, function(error, response, body) {
+
+
+        // If the request is successful
+        var jdata = JSON.parse(body);
+        //  console.log(response);
+        //	console.log(jdata);
+        if (!error && response.statusCode == 200) {
+
+            var mTitle = jdata.Title;
+            var mReleased = jdata.Released;
+            var mImdb = jdata.imdbRating;
+            var mCountry = jdata.Country;
+            var mLanguage = jdata.Language;
+            var mPlot = jdata.Plot;
+            var mActor = jdata.Actors;
+            var mRottonTomatos = JSON.stringify(jdata.Ratings[0].Value);
+            console.log("\n************************************************");
+            console.log("Title: " + mTitle, "\nReleased: " + mReleased, "\nIMDB Ratings: " + mImdb, "\nCountry: " + mCountry, "\nLanguage: " + mLanguage, "\nPlot: " + mPlot, "\nActors: " + mActor, "\nRotton Tomatoes: " + mRottonTomatos);
+            console.log("************************************************");
+
+        }
+    });
+    // ...
+    myLoop();
+}
+
+
+
+// ************************************ Spotify Function
+function chkspotify(newSearch, request) {
+    process.stdout.write('\033c');
+
+    var spotify = require('spotify');
+    if (newSearch == "") {
+        newSearch = 'dancing in the moonlight';
+
+    }
+
+
+    spotify.search({
+        type: 'track',
+        query: newSearch
+    }, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        } else {
+            var jdata = JSON.stringify(data, null, 4);
+            console.log(jdata);
+        }
+
+        // Do something with 'data' 
+    });
+    myLoop();
+}
+
+
+// ******************************** Function to replace '+' sign in place of spaces
+
+function checkarg(xnum) {
+
+
+    if (xnum == "1") {
+        inquirer.prompt([{
+            type: "input",
+            name: "newSearch1",
+            message: "Enter name of Movie to search?"
+
+
+        }, ]).then(function(user) {
+
+            newSearch = user.newSearch1.split(' ').join('+');
+            chkmovie(newSearch, request);
+
+        })
+    }
+    if (xnum == "2") {
+
+        inquirer.prompt([{
+            type: "input",
+            name: "newSearch1",
+            message: "Enter name of Song to search?"
+
+
+        }, ]).then(function(user) {
+            newSearch = user.newSearch1.split(' ').join('+');
+            chkspotify(newSearch, request);
+        })
+    }
+
+}
+
+//------------------ End of Code
